30cd49c2bb6914159c937964a53075933966b131
kyle1563 <kyle.vu0607@gmail.com>
2025-04-14 09:52:43 -0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'React-App-Kyle-Vu', //Replace with your GitHub repo name
+});
+
